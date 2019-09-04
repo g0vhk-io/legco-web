13b9b8aaa181de1e37d45348b84fed92c3af3d04
Ho Wa Wong <howa.wong@gmail.com>
2019-09-04 23:37:38 +0800

    Index Pafe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+function CustomNavBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">立法會議員資料庫</Navbar.Brand>
+    </Navbar>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {<CustomNavBar />}
+        <Container>
+          <Row>
+            <Col><Button variant="primary">新界東</Button></Col>
+            <Col><Button variant="secondary">新界西</Button></Col>
+          </Row>
+          <Row>
+            <Col><Button variant="success">九龍東</Button></Col>
+            <Col><Button variant="warning">九龍西</Button></Col>
+          </Row>
+          <Row>
+            <Col><Button variant="danger">港島</Button></Col>
+            <Col><Button variant="info">功能</Button></Col>
+          </Row>
+        </Container>
       </header>
     </div>
   );
